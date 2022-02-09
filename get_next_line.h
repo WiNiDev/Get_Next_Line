b789826bacf30def9bd1d347d888e6e288b7c5c0
WiNiDev <97839558+WiNiDev@users.noreply.github.com>
2022-02-09 18:30:33 +0100

    Initial Commit

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/11 13:36:26 by marvin            #+#    #+#             */
+/*   Updated: 2022/01/31 14:42:02 by mberthoi         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+int		ft_strlen(char const *str);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
